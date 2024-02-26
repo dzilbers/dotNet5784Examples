ed9d9f7a965dedd543427f3a5978c83fa817c01b
Dan Zilberstein <dzilbers@gmail.com>
2024-02-26 08:41:54 +0200

    Add attributes lesson

---
+++ Attributes/Program.cs
@@ -0,0 +1,23 @@
+﻿namespace Attributes;
+using System.Diagnostics;
+
+//[DebuggerDisplay("{Name,nq}({Id,h})")]
+class Person
+{
+    internal int Id;
+    internal string Name;
+//    [Obsolete("Please stop using this function, use f2(int) instead", false)]
+    internal void f1() { }
+    internal void f2(int num) { }
+//    public override string ToString() => $"Person: #{Id}, name: {Name}";
+}
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Person p = new Person { Id = 123, Name = "Shira" };
+        p.f1();
+        Console.WriteLine(p);
+    }
+}
