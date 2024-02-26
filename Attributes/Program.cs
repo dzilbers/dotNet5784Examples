2fc2530850a390f06537e9cdab47f15030578a32
dzilbers <dzilbers@gmail.com>
2024-02-26 09:05:05 +0200

    Update Attributes with top-level statements and nullable string field according to modern C#

--- Attributes/Program.cs
+++ Attributes/Program.cs
@@ -1,23 +1,28 @@
-﻿namespace Attributes;
-using System.Diagnostics;
+﻿// using System.Diagnostics;
 
+// Main:
+Person p = new Person { Id = 123, Name = "Shira" };
+p.F1();
+Console.WriteLine(p);
+
+/// <summary>
+/// Person class for demonstration of attributes:
+/// <ul>
+/// <li>DebuggerDisplay</li>
+/// <li>Obsolete</li>
+/// </ul>
+/// </summary>
 //[DebuggerDisplay("{Name,nq}({Id,h})")]
 class Person
 {
     internal int Id;
-    internal string Name;
-//    [Obsolete("Please stop using this function, use f2(int) instead", false)]
-    internal void f1() { }
-    internal void f2(int num) { }
-//    public override string ToString() => $"Person: #{Id}, name: {Name}";
-}
+    internal string? Name;
 
-class Program
-{
-    static void Main(string[] args)
-    {
-        Person p = new Person { Id = 123, Name = "Shira" };
-        p.f1();
-        Console.WriteLine(p);
-    }
+#pragma warning disable CA1822 // Mark members as static
+    //    [Obsolete("Please stop using this function, use f2(int) instead", false)]
+    internal void F1() { }
+    internal void F2(int num) { }
+#pragma warning restore CA1822 // Mark members as static
+
+//    public override string ToString() => $"Person: #{Id}, name: {Name}";
 }
