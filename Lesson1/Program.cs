522e825acea090a86bb4df4eb4f155efd8458460
dzilbers <dzilbers@gmail.com>
2024-03-20 18:46:20 +0200

    YATMAL lesson 1

--- Lesson1/Program.cs
+++ Lesson1/Program.cs
@@ -1,5 +1,14 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Hello, World!");
-Test.MyClass obj = new();
+Test.TestClass obj = new();
 obj.id = 3;
 obj.print();
+
+for (int i = 0; i < 10; i++) ;
+
+Console.WriteLine("Another hello to you!");
+
+if (!int.TryParse(Console.ReadLine(), out int input))
+    Console.WriteLine("Wrong input");
+else
+    Console.WriteLine($"Number: {input}");
