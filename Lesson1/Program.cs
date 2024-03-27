267b8a7f4a7dcc535065e6ebfc8234f1b46de132
Dan Zilberstein <dzilbers@gmail.com>
2024-03-27 20:09:53 +0200

    lesson 2 with yatmal

--- Lesson1/Program.cs
+++ Lesson1/Program.cs
@@ -2,4 +2,15 @@
 Console.WriteLine("Hello, World!");
 Test.MyClass obj = new();
 obj.id = 3;
-obj.print();
+// obj.print();
+
+string str1 = "abcdef";
+string str2 = str1;
+Console.WriteLine(str1[3]);
+// str1[3] = 'a';
+str1 += "ghij";
+Console.WriteLine(str1);
+Console.WriteLine(str2);
+
+Object obj2 = new();
+
--- Lesson2/Program.cs
+++ Lesson2/Program.cs
@@ -1,51 +1,224 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
-int number = (8);
-var test = 2.5;
-uint positive = (uint) number;
-Console.WriteLine(number);
+using System.Reflection;
 
-MyClass.Gender a = MyClass.Gender.Male;
-var obj = new MyClass() { MyGender = MyClass.Gender.Male };
-// obj.MyGender = a;
-Console.WriteLine(obj.MyGender);
-Console.WriteLine(obj.Func1("something", 4, 8, 11, 2));
-
-obj.Func2(par2: "Dani", par1: 5, par3: 2.5);
-
-MyRecord rec1 = new(123, "Yossi");
-MyRecord rec2 = rec1 with { Number = 877 };
-Console.WriteLine(rec1);
-Console.WriteLine(rec2);
-rec2.Family = 8;
-
-int? nullableNumber = null;
-
-int num = 1;
-func(ref num);
-Console.WriteLine(num);
-
-static void func(ref int i)
+class Program
 {
-    Console.WriteLine(i);
-    i = 8;
-}
+    public static void Main()
+    {
+        MyClass test1 = new();
+        MyRecord test2 = new(7, "Dani");
+        Console.WriteLine(test1);
+        Console.WriteLine(test2);
+        Console.ReadKey();
 
+        printInfo("", typeof(MyRecord));
+        Console.Write("Press any key...");
+        Console.ReadKey();
+
+        Console.WriteLine(" ID:{0} called {1}, gender={2}", 4, "Yossi", MyClass.Genders.Female);
+        Console.Write("Press any key...");
+        Console.ReadKey();
+
+        MyClass myObj = new();
+        // Console.WriteLine(myObj.get_Number());
+        Console.WriteLine(myObj.Number);
+
+        ValueType obj9 = MyClass.Genders.Male;
+        Console.WriteLine(obj9);
+
+        object objA = obj9;
+
+        Console.WriteLine("Hello, World!");
+        int number = (8);
+        var test = 2.5;
+        uint positive = (uint)number;
+        Console.WriteLine(number);
+
+        positive = 38u;
+
+        MyClass.Genders a = MyClass.Genders.Male;
+        var obj = new MyClass() { Gender = MyClass.Genders.Male };
+        obj.Gender = a;
+        Console.WriteLine(obj.Gender);
+        Console.WriteLine(obj.Func1("something", 4, 8, 11, 2));
+
+        obj.Func2(par2: "Dani", par1: 5, par3: 2.5);
+
+        MyRecord rec1 = new(123, "Yossi");
+        MyRecord rec3 = rec1;
+        MyRecord rec2 = rec1 with { Number = 877 };
+        Console.WriteLine(rec1);
+        Console.WriteLine(rec2);
+        rec2.Family = 8;
+
+        int? nullableNumber = null;
+
+        int num = 1;
+        nullableNumber = num;
+        num = nullableNumber ?? 0;
+        num = nullableNumber!.Value;
+        var check = nullableNumber!.HasValue;
+
+        func(ref num);
+        Console.WriteLine(num);
+    }
+    static void func(ref int i)
+    {
+        Console.WriteLine(i);
+        i = 8;
+    }
+
+    static string accessLevel(FieldInfo item) => (item.IsInitOnly ? ", readonly" : "") + ", access: " +
+        item switch
+        {
+            { IsPrivate: true } => "private",
+            { IsPublic: true } => "public",
+            { IsFamily: true } => "protected",
+            { IsFamilyAndAssembly: true } => "internal private",
+            { IsFamilyOrAssembly: true } => "private protected",
+            { IsAssembly: true } => "internal",
+            _ => ""
+        };
+    static string accessLevel(MethodInfo item) => ", access: " +
+    item switch
+    {
+        { IsPrivate: true } => "private",
+        { IsPublic: true } => "public",
+        { IsFamily: true } => "protected",
+        { IsFamilyAndAssembly: true } => "internal private",
+        { IsFamilyOrAssembly: true } => "private protected",
+        { IsAssembly: true } => "internal",
+        _ => ""
+    };
+    static string accessLevel(ConstructorInfo item) => ", access: " +
+    item switch
+    {
+        { IsPrivate: true } => "private",
+        { IsPublic: true } => "public",
+        { IsFamily: true } => "protected",
+        { IsFamilyAndAssembly: true } => "internal private",
+        { IsFamilyOrAssembly: true } => "internal protected",
+        { IsAssembly: true } => "internal",
+        _ => ""
+    };
+
+    static void printInfo(string suffix, Type type)
+    {
+        string category = type.IsValueType ? "ValueType" : "ReferenceType";
+        Console.WriteLine(suffix + $"""Type Name: {type.Name}: {category} {type switch { { IsInterface: true } => "interface", { IsEnum: true } => "enum", { IsClass: true } => "class", _ => "" }} {type switch { { IsAbstract: true } => "absract", { IsSealed: true } => "sealed", _ => "" }} {type switch { { IsGenericType: true } => "generic", _ => "" }}""");
+
+        Console.WriteLine(suffix + "Base Type: ");
+        if (type.BaseType == null)
+            Console.WriteLine(suffix + suffix + "None");
+        else
+            printInfo(suffix + "  ", type.BaseType);
+
+        FieldInfo[] staticFields = type.GetFields(BindingFlags.Public | BindingFlags.NonPublic | BindingFlags.Static);
+        FieldInfo[] instanceFields = type.GetFields(BindingFlags.Public | BindingFlags.NonPublic | BindingFlags.Instance);
+        PropertyInfo[] staticProperties = type.GetProperties(BindingFlags.Public | BindingFlags.NonPublic | BindingFlags.Static);
+        PropertyInfo[] instanceProperties = type.GetProperties(BindingFlags.Public | BindingFlags.NonPublic | BindingFlags.Instance);
+        MethodInfo[] staticMethods = type.GetMethods(BindingFlags.Public | BindingFlags.NonPublic | BindingFlags.Static);
+        MethodInfo[] instanceMethods = type.GetMethods(BindingFlags.Public | BindingFlags.NonPublic | BindingFlags.Instance);
+        ConstructorInfo[] staticConstructors = type.GetConstructors(BindingFlags.Public | BindingFlags.NonPublic | BindingFlags.Static);
+        ConstructorInfo[] instanceConstructors = type.GetConstructors(BindingFlags.Public | BindingFlags.NonPublic | BindingFlags.Instance);
+
+        Console.WriteLine(suffix + "Member Info:");
+        if (staticFields.Length > 0)
+        {
+            Console.WriteLine(suffix + " Static fields:");
+            foreach (var item in staticFields)
+                Console.WriteLine(suffix + "  name: {0,-32} type: {1,-11} in: {2}" +
+                    accessLevel(item) + (item.IsLiteral ? " literal" : ""), item.Name, item.MemberType, item.DeclaringType?.Name);
+        }
+        if (instanceFields.Length > 0)
+        {
+            Console.WriteLine(suffix + " Instance fields:");
+            foreach (var item in instanceFields)
+                Console.WriteLine(suffix + "  name: {0,-32} type: {1,-11} in: {2}" +
+                    accessLevel(item) + (item.IsLiteral ? " literal" : ""), item.Name, item.MemberType, item.DeclaringType?.Name);
+        }
+
+        if (staticProperties.Length > 0)
+        {
+            Console.WriteLine(suffix + " Static properties:");
+            foreach (var item in staticProperties)
+            {
+                Console.Write(suffix + "    name: {0,-32} type: {1,-11} in: {2}",
+                                  item.Name, item.MemberType, item.DeclaringType?.Name);
+                Console.WriteLine((item.CanRead ? ", GET" + accessLevel(item.GetGetMethod()!) : "") +
+                                  (item.CanWrite ? ", SET" + accessLevel(item.GetSetMethod()!) : ""));
+            }
+        }
+        if (instanceFields.Length > 0)
+        {
+            Console.WriteLine(suffix + " Instance properties:");
+            foreach (var item in instanceProperties)
+            {
+                Console.Write(suffix + "  name: {0,-32} type: {1,-11} in: {2}",
+                                  item.Name, item.MemberType, item.DeclaringType?.Name);
+                Console.WriteLine((item.CanRead ? ", GET" + accessLevel(item.GetGetMethod()!) : "") +
+                                  (item.CanWrite ? ", SET" + accessLevel(item.GetSetMethod()!) : ""));
+            }
+        }
+
+        if (staticMethods.Length > 0)
+        {
+            Console.WriteLine(suffix + " Static methods:");
+            foreach (var item in staticMethods)
+                Console.WriteLine(suffix + "  name: {0,-32} type: {1,-11} in: {2}" + accessLevel(item),
+                                  item.Name, item.MemberType, item.DeclaringType?.Name);
+        }
+        if (instanceMethods.Length > 0)
+        {
+            Console.WriteLine(suffix + " Instance methods:");
+            foreach (var item in instanceMethods)
+                Console.WriteLine(suffix + "  name: {0,-32} type: {1,-11} in: {2}" + accessLevel(item),
+                                  item.Name, item.MemberType, item.DeclaringType?.Name);
+        }
+
+        if (staticConstructors.Length > 0)
+        {
+            Console.WriteLine(suffix + " Static constructors:");
+            foreach (var item in staticConstructors)
+                Console.WriteLine(suffix + "  name: {0,-32} type: {1,-11} in: {2}" + accessLevel(item),
+                                  item.Name, item.MemberType, item.DeclaringType?.Name);
+        }
+        if (instanceConstructors.Length > 0)
+        {
+            Console.WriteLine(suffix + " Instance constructors:");
+            foreach (var item in instanceConstructors)
+                Console.WriteLine(suffix + "  name: {0,-32} type: {1,-11} in: {2}" + accessLevel(item),
+                                  item.Name, item.MemberType, item.DeclaringType?.Name);
+        }
+
+        //MemberInfo[] members = type.GetMembers((BindingFlags)0x7FFF);
+        //foreach (var item in members)
+        //    Console.WriteLine(suffix + "name: {0,-32} type: {1,-11} in: {2}",
+        //                      item.Name, item.MemberType, item.DeclaringType?.Name);
+    }
+}
 
 record MyRecord(int Number, string Name)
 {
     public int Family = 1;
 }
 
+class Try(int number)
+{
+    public int Number = number;
+}
+
 /// <summary>
 /// f ssf ds  d d d gadfg dfg
 /// </summary>
-class MyClass
+public class MyClass
 {
-    public enum Gender { Male, Female }
+    public enum Genders { Male, Female }
 
-    private Gender gender = Gender.Female;
-    public Gender MyGender { get => gender; set => gender = value; }
+    private Genders _gender = Genders.Female;
+    public Genders Gender { get => _gender; set => _gender = value; }
+
+    public DateTime Today { get => DateTime.Now; }
 
     public int Number { get; set; } = 8;
 
