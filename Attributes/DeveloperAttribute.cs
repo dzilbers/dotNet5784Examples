a92c8d4f51d03a2e237820ed0ac146442e8127d7
Dan Zilberstein <dzilbers@gmail.com>
2025-01-13 10:49:22 +0200

    add attributes stuff

---
+++ Attributes/DeveloperAttribute.cs
@@ -0,0 +1,35 @@
+﻿namespace Attributes;
+
+/// <summary>
+/// This constructor defines two required parameters: name and level.
+/// <summary>
+[AttributeUsage(AttributeTargets.Class | AttributeTargets.Method, AllowMultiple = true)]
+public class DeveloperAttribute(string name, int level) : Attribute
+{
+    // Private fields.
+    private readonly string _name = name;
+    private readonly int _level = level;
+    private bool _reviewed = false;
+
+    /// <summary>
+    /// Define Name property.
+    /// This is a read-only attribute.
+    /// </summary>
+    public string Name => _name;
+
+    /// <summary>
+    /// Define Level property.
+    /// This is a read-only attribute.
+    /// </summary>
+    public virtual int Level => _level;
+
+    /// <summary>
+    /// Define Reviewed property.
+    /// This is a read/write attribute.
+    /// </summary>
+    public virtual bool Reviewed
+    {
+        get => _reviewed;
+        set => _reviewed = value;
+    }
+}
--- Attributes/Program.cs
+++ Attributes/Program.cs
@@ -1,12 +1,106 @@
-﻿#pragma warning disable CA1822 // Mark members as static
+﻿#pragma warning disable IDE0079 // Remove unnecessay suppression
+#pragma warning disable CA1822 // Mark members as static
 #pragma warning disable IDE0060 // Remove unused parameter
 
 using System.Diagnostics;
+using System.Reflection;
 
-// Main:
-Person p = new() { Id = 123, Name = "Shira" };
-p.F1();
-Console.WriteLine(p);
+namespace Attributes;
+
+[Developer("Dani", 58, Reviewed = true)]
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        // Main:
+        Person p = new() { Id = 123, Name = "Shira" };
+        p.F1();
+        Console.WriteLine(p);
+
+        GetAttribute(typeof(Program));
+    }
+
+    [Developer("Dani", 58, Reviewed = true)]
+    [Developer("Yossi", 51)]
+    public static void GetAttribute(Type t)
+    {
+        // Get instance of the attribute.
+        if (Attribute.GetCustomAttribute(t, typeof(DeveloperAttribute)) is DeveloperAttribute att1)
+        {
+            // Get the Name value.
+            Console.WriteLine($"The Name is: {att1.Name}.");
+            // Get the Level value.
+            Console.WriteLine($"The Level is: {att1.Level}.");
+            // Get the Reviewed value.
+            Console.WriteLine($"The Reviewed is: {att1.Reviewed}.");
+        }
+        else
+            Console.WriteLine("The attribute was not found.");
+
+        Console.WriteLine("================================");
+        if (t.GetCustomAttribute(typeof(DeveloperAttribute)) is DeveloperAttribute att2)
+        {
+            // Get the Name value.
+            Console.WriteLine($"The Name is: {att2.Name}.");
+            // Get the Level value.
+            Console.WriteLine($"The Level is: {att2.Level}.");
+            // Get the Reviewed value.
+            Console.WriteLine($"The Reviewed is: {att2.Reviewed}.");
+        }
+
+        Console.WriteLine("================================");
+        var att3 = t.GetCustomAttribute<DeveloperAttribute>();
+        if (att3 is not null)
+        {
+            // Get the Name value.
+            Console.WriteLine($"The Name is: {att3.Name}.");
+            // Get the Level value.
+            Console.WriteLine($"The Level is: {att3.Level}.");
+            // Get the Reviewed value.
+            Console.WriteLine($"The Reviewed is: {att3.Reviewed}.");
+        }
+
+        Console.WriteLine("======== Attribute data:");
+        foreach (var data in t.GetCustomAttributesData())
+        {
+            Console.WriteLine($"  Attribute type is {data.AttributeType}");
+            Console.WriteLine("    ========= Arguments:");
+            foreach (var arg in data.ConstructorArguments)
+                Console.WriteLine($"    ({arg.ArgumentType}){arg.Value}");
+            Console.WriteLine("    ========= Named Arguments:");
+            foreach (var arg in data.NamedArguments)
+                Console.WriteLine($"    {arg.MemberName} = {arg.TypedValue}");
+        }
+
+
+        Console.WriteLine("======== Method attributes:");
+        // Get the method-level attributes.
+
+        // Get all methods in this class, and put them
+        // in an array of System.Reflection.MemberInfo objects.
+        MemberInfo[] myMemberInfo = t.GetMethods(BindingFlags.DeclaredOnly | BindingFlags.Public | BindingFlags.NonPublic | BindingFlags.Static);
+
+        // Loop through all methods in this class that are in the
+        // MyMemberInfo array.
+        foreach (var member in myMemberInfo)
+        {
+            Console.WriteLine($"Interrogating member {member.Name} ({member}):");
+            // Get instance of the attribute.
+            foreach(DeveloperAttribute att in member.GetCustomAttributes<DeveloperAttribute>())
+            {
+                Console.WriteLine("  ===========================");
+                // Get the Name value.
+                Console.WriteLine($"  The Name is: {att.Name}.");
+                // Get the Level value.
+                Console.WriteLine($"  The Level is: {att.Level}.");
+                // Get the Reviewed value.
+                Console.WriteLine($"  The Reviewed is: {att.Reviewed}.");
+            }
+            Console.WriteLine("  ===========================");
+        }
+
+    }
+}
 
 /// <summary>
 /// Person class for demonstration of attributes:
@@ -33,3 +127,4 @@ class Person
 
 #pragma warning restore IDE0060 // Remove unused parameter
 #pragma warning restore CA1822 // Mark members as static
+#pragma warning restore IDE0079 // Remove unnecessay suppression
